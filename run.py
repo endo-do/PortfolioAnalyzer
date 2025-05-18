c97b53e8b4def3d22434b8c4e64ed1f743874415
endodo <anmelderhuhnx@mailbox.org>
2025-05-18 18:03:16 +0200

    enhanced project structure

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from flask import Flask, render_template
 from app import create_app
 
 app = create_app()
+
+@app.route("/")
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
